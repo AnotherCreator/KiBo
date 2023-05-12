db3dd41ba09709323653a99be9d7e893a58c21b9
Josh R <jreginaldo@protonmail.com>
2023-05-12 14:26:02 -0500

    Create helper function for embed generation and add definition functionality

--- src/modules/dictionary.py
+++ src/modules/dictionary.py
@@ -10,15 +10,32 @@ logger = logging.getLogger()
 plugin = lightbulb.Plugin("dictionary")  # Create plugin
 
 
+# ----------    HELPER FUNCTIONS    ---------- #
+def create_dictionary_embed(user_input, dictionary_results) -> hikari.Embed():
+    embed = (
+        hikari.Embed(
+            title=f"{user_input}"
+        )
+    )
+
+    for key, value in dictionary_results.items():
+        embed.add_field(
+            key,
+            value,
+            inline=False
+        )
+
+    return embed
+
 # ----------    BOT COMMANDS    ---------- #
 @plugin.command
-@lightbulb.command("dictionary", "Use the dictionary")
+@lightbulb.command("book", "A list of books")
 @lightbulb.implements(lightbulb.SlashCommandGroup)
 async def book(ctx: lightbulb.Context) -> None:
     pass
 
 
-@book.child
+@book.child()
 @lightbulb.option("translate", "Translate a word into another language",
                   required=False, type=str, default=None, min_length=1)
 @lightbulb.option("antonym", "Find the antonym of a word",
@@ -31,26 +48,24 @@ async def book(ctx: lightbulb.Context) -> None:
 @lightbulb.implements(lightbulb.SlashSubCommand)
 async def use_dictionary(ctx: lightbulb.Context) -> None:
     # Get user input
-    user_translate = ctx.options.translate
     user_antonym = ctx.options.antonym
     user_synonym = ctx.options.synonym
     user_definition = ctx.options.define
 
+    embed = hikari.Embed()  # Instantiate embed obj
+
     if user_definition is not None:
-        logger.info(f"User definition: {dictionary.meaning(user_definition)}")
+        logger.info(f"User definition: {user_definition}:{dictionary.meaning(user_definition)}")
+        embed = create_dictionary_embed(user_definition, dictionary.meaning(user_definition))
 
-    if user_synonym is not None:
-        logger.info(f"User synonym: {dictionary.synonym(user_synonym)}")
+    # if user_synonym is not None:
+    #     logger.info(f"User synonym: {user_synonym}:{dictionary.synonym(user_synonym)}")
+    #     embed = create_dictionary_embed(user_synonym, dictionary.synonym(user_synonym))
+    #
+    # if user_antonym is not None:
+    #     logger.info(f"User antonym: {user_antonym}:{dictionary.antonym(user_antonym)}")
+    #     embed = create_dictionary_embed(user_antonym, dictionary.antonym(user_antonym))
 
-    if user_antonym is not None:
-        logger.info(f"User antonym: {dictionary.antonym(user_antonym)}")
-
-    if user_translate is not None:
-        logger.info(f"User translation: {dictionary.translate(user_translate, 'tl')}")
-
-    embed = (
-        hikari.Embed(title="Hi").add_field("Hi", "Hi", inline=False).add_field("2nd Hi", "2nd Hi", inline=False)
-    )
     await ctx.respond(embed)
 
 
