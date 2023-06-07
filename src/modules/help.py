7cbc241bbeedba09515d0def56f974e1526153e7
Josh R <jreginaldo@protonmail.com>
2023-06-07 16:45:47 -0500

    Create sample output for '/help' command

--- src/modules/help.py
+++ src/modules/help.py
@@ -1,14 +1,29 @@
+import hikari
 import lightbulb
 
 plugin = lightbulb.Plugin("help")
 
 
 # ----------    BOT COMMANDS    ---------- #
-# @plugin.command
-# @lightbulb.command("market", "response!")
-# @lightbulb.implements(lightbulb.SlashCommand)
-# async def ping(ctx):
-#     await ctx.respond("response!")
+@plugin.command
+@lightbulb.command("help", "Command information")
+@lightbulb.implements(lightbulb.SlashCommand)
+async def help_command(ctx: lightbulb.Context) -> None:
+    embed = (
+        hikari.Embed(
+            title=f"Hi"
+        )
+        .set_author(
+            name=f"Hi"
+        )
+        .add_field(
+            "Daily % Change",
+            f"Among",
+            inline=False
+        )
+    )
+
+    await ctx.respond(embed)
 
 
 # ----------    LOAD PLUG-IN   ---------- #
